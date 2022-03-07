d140e08dd46d4d7c21baa7d7486b718d91dedb4b
seanprehensible <seanprehensible@gmail.com>
2022-03-07 10:31:40 +0900

    Add Boilerplate Code with hot reloading

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # slack-backend
+
 슬랙 클론코딩 백엔드 서버
+
+[개발 환경 설정](Note/%EA%B0%9C%EB%B0%9C-%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95.md)
---
+++ webpack-hmr.config.js
@@ -0,0 +1,23 @@
+/* 핫 리로딩 설정 */
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+    entry: ['webpack/hot/poll?100', options.entry],
+    externals: [
+      nodeExternals({
+        allowlist: ['webpack/hot/poll?100'],
+      }),
+    ],
+    plugins: [
+      ...options.plugins,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.WatchIgnorePlugin({
+        paths: [/\.js$/, /\.d\.ts$/],
+      }),
+      new RunScriptWebpackPlugin({ name: options.output.filename }),
+    ],
+  };
+};
